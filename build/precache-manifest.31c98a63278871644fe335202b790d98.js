2b8a29bd1bacb87247aaa173c04e4afa18834c95
Rohin Kumar <74658489+Rohinkumar1841@users.noreply.github.com>
2021-12-28 23:42:58 +0530

    Add files via upload

---
+++ build/precache-manifest.31c98a63278871644fe335202b790d98.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c55fcd43d7c99fda0f1e6a616915bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d9791ef6a52f72ab9a3",
+    "url": "/static/js/2.8a00a5ce.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.8a00a5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd746238a213e54b8334",
+    "url": "/static/js/main.963b6317.chunk.js"
+  },
+  {
+    "revision": "5dcc4a2d51d1022f96e5",
+    "url": "/static/js/runtime-main.edb82d6c.js"
+  },
+  {
+    "revision": "e485817ffce982e3b03046905476be46",
+    "url": "/static/media/child.e485817f.svg"
+  },
+  {
+    "revision": "ba5dfdd142fdb858c6594954bef03ca7",
+    "url": "/static/media/communicate.ba5dfdd1.svg"
+  },
+  {
+    "revision": "deaa4fa4a31a6dfbadfa6f412e075306",
+    "url": "/static/media/healthcare.deaa4fa4.svg"
+  },
+  {
+    "revision": "3a007c8b431a7ec595cd4cfd301076da",
+    "url": "/static/media/jj.3a007c8b.webp"
+  },
+  {
+    "revision": "a69a151424df0dd81009aa8b1e62ef78",
+    "url": "/static/media/oo.a69a1514.png"
+  },
+  {
+    "revision": "f9c92da170c71808985561164cfbae23",
+    "url": "/static/media/reminder.f9c92da1.svg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/static/media/svg-2.d4846e11.svg"
+  }
+]);
